b7fdaaab4abcba920cde90068642fb2d383c3fad
Saidul Arefin <114782285+Saidul-Arefin@users.noreply.github.com>
2023-11-08 17:51:49 +0600

    random

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "https://bookshelf-client-d412a.web.app",
+      "https://bookshelf-client-d412a.firebaseapp.com",
+    ],
     credentials: true,
   })
 );
@@ -80,8 +83,6 @@ async function run() {
     });
 
     app.get("/api/v1/books", verifyToken, async (req, res) => {
-      
-
       let query = {};
       if (req.query?.category) {
         query = { category: req.query.category };
@@ -97,7 +98,7 @@ async function run() {
     });
     app.patch("/api/v1/books/:id", async (req, res) => {
       const id = req.params.id;
-      
+
       const filter = { _id: new ObjectId(id) };
       const updatedBookInfo = req.body;
 
